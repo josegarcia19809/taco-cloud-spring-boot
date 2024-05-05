f7039aebe983fd6fbae4560d3b6368e22c578220
josegarcia19809 <josegarcia19809@gmail.com>
2024-05-04 22:29:22 -0600

    Commit inicial

---
+++ src/main/java/sistemas/tacocloud/TacoCloudApplication.java
@@ -0,0 +1,13 @@
+package sistemas.tacocloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacoCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TacoCloudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sistemas/tacocloud/TacoCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package sistemas.tacocloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacoCloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
